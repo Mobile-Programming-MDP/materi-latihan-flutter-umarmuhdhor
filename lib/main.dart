6418df463d02d961bc607a84f03faaacac979cb1
Umar Muhdhor <muhdhorcs@gmail.com>
2023-10-16 17:04:36 +0700

    Tugas di PPT

--- lib/main.dart
+++ lib/main.dart
@@ -37,8 +37,50 @@ class firstScreen extends StatelessWidget {
           icon: const Icon(Icons.home, color: Colors.white),
         ),
       ),
-      body: const Center(
-        child: Text("Halo Umar"),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Image(image: NetworkImage("https://picsum.photos/id/88/300/200"))
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.all(20),
+            child: Center(
+              child: Text("Jalan Di Barcelona",style: TextStyle(fontWeight: FontWeight.bold),)
+          )),
+          Container(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.place, color: Colors.orange)),
+                    Text("Lokasi Barcelona Spanyol")
+                  ],
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.calendar_month,
+                          color: Colors.lightBlue,
+                        )),
+                    Text("Publikasi: 13 Agustus 2023")
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text("Deskripsi",style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text("Sebuah persimpangan jalan di Barcelona, Spanyol. Foto ini menampilkan berbagai kendaraan yang bergerak dalam arah yang berbeda, menciptakan pemandangan yang sibuk dan energik.",)
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
