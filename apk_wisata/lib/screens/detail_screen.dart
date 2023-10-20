e34683c023d2748f5b89efb3a7fd24568a566211
Umar Muhdhor <muhdhorcs@gmail.com>
2023-10-20 18:31:23 +0700

    Pembuatan Home Screen

---
+++ apk_wisata/lib/screens/detail_screen.dart
@@ -0,0 +1,50 @@
+import 'package:apk_wisata/models/candi.dart';
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  final Candi candi;
+
+  const DetailScreen({super.key, required this.candi});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: Image.asset(
+                    candi.imageAsset,
+                    width: double.infinity,
+                    height: 300,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+              Padding(
+                padding:const EdgeInsets.symmetric(horizontal: 16,vertical: 32),
+                 child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple[100]?.withOpacity(0.8),
+                    shape: BoxShape.circle
+                  ),
+                  child: IconButton(
+                    onPressed: (){},
+                    icon: const Icon(
+                      Icons.arrow_back
+                    ),
+                  ),
+                 ),
+                )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
