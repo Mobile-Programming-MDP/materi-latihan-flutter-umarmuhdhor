7368ad3681fa3f52963f8555a5c6f22db42265f6
Umar Muhdhor <muhdhorcs@gmail.com>
2023-11-13 18:13:01 +0700

    Navbar

--- apk_wisata/lib/main.dart
+++ apk_wisata/lib/main.dart
@@ -1,9 +1,11 @@
-import 'package:apk_wisata/data/candi_data.dart';
-// import 'package:apk_wisata/screens/detail_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:apk_wisata/screens/favorite_screen.dart';
+import 'package:apk_wisata/screens/homeScreen.dart';
+// import 'package:wisata_candi/data/candi_data.dart';
+// import 'package:wisata_candi/screens/detail_screen.dart';
 import 'package:apk_wisata/screens/profile_screen.dart';
 import 'package:apk_wisata/screens/search_screen.dart';
-import 'package:apk_wisata/screens/sign_in.dart';
-import 'package:flutter/material.dart';
+// import 'package:wisata_candi/screens/SignInScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,19 +14,104 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  Widget build(BuildContext context){
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Wisata Candi',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true
+        appBarTheme: const AppBarTheme(
+          iconTheme: IconThemeData(color: Colors.deepPurple),
+          titleTextStyle: TextStyle(
+            color: Colors.deepPurple,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
+          primary: Colors.deepPurple,
+          surface: Colors.deepPurple[50],
+        ),
+        useMaterial3: true,
       ),
-      // home : DetailScreen(candi : candiList[0])
-      home: SearchScreen(),
+//home: HomeScreen(),
+//home: const SearchScreen(),
+//home: SignInScreen(),
+//home: DetailScreen(candi: candiList[0]),
+      home: MainScreen(),
     );
   }
-
 }
 
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
 
-  
+  @override
+  State createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State {
+//TOD: 1 Deklarasikan variabel
+  int _currentIndex = 0;
+  final List _children = [
+    HomeScreen(),
+    SearchScreen(),
+    FavoriteScreen(),
+    ProfileScreen(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+//TODO: 2 Buat properti body berupa widget yang ditampilkan
+      body: _children[_currentIndex],
+//TODO: 3 Buat properti bottonNavigationBar dengan nilai Theme
+      bottomNavigationBar: Theme(
+//TODO: 4 Buat data dan child dari Theme
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.deepPurple[50],
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: Colors.deepPurple,
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                color: Colors.deepPurple,
+              ),
+              label: 'Seacrh',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite,
+                color: Colors.deepPurple,
+              ),
+              label: 'Favorite',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+                color: Colors.deepPurple,
+              ),
+              label: 'Person',
+            ),
+          ],
+          selectedItemColor: Colors.deepPurple,
+          unselectedItemColor: Colors.deepPurple[100],
+          showUnselectedLabels: true,
+        ),
+      ),
+    );
+  }
+}
